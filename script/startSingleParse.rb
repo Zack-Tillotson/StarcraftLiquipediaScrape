adc25875e4aaee2588445d6aae29a3cb50795d48
Zack Tillotson <zachery.tillotson@echostar.com>
2013-01-07 16:53:33 -0700

    Added 2 more examples

--- script/startSingleParse.rb
+++ script/startSingleParse.rb
@@ -4,3 +4,5 @@ require 'parse/SingleElimBracketParser.rb'
 
 parser = StarcraftLiquipediaScrape::SingleElimBracketParser.new('mlgspringobwseeds', 'data/mlgspringobwseeds.txt')
 parser = StarcraftLiquipediaScrape::SingleElimBracketParser.new('se8bracket', 'data/8sebracket.txt')
+parser = StarcraftLiquipediaScrape::SingleElimBracketParser.new('nasl4', 'data/nasl4.txt')
+parser = StarcraftLiquipediaScrape::SingleElimBracketParser.new('homestory6', 'data/homestorycup6.txt')
